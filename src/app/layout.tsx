b949d19cacfcac1595fe701939c79e397e074aef
nemzzcobain-debug <nemzzcobain@gmail.com>
2026-07-25 14:30:54 +0200

    [feat] utilise le nouveau logo sur l’écran d’accueil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,11 +36,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: '32x32' },
-      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/icons/icon-318-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/icon-318-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/apple-touch-icon-318.png', sizes: '180x180', type: 'image/png' },
     ],
   },
   openGraph: {
